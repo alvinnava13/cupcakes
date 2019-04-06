b35918eaaa94f412c82abc6a3f8382cb9ef744ae
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-06 00:42:55 -0400

    Start cupcake ordering form with header and name input

---
+++ cupcake.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcakes</title>
+</head>
+<body>
+
+<h1>Cupcake Fundraiser</h1>
+
+<form action="cupcake.php" method="POST" id="form">
+    <label>Your Name: <br><input type="text" name="name" size="50" maxlength="50" value="
+    <?php if (isset($_POST['name'])) echo $_POST['name']; ?>"></label><br>
+    <label><p>Cupcake Flavors:</p>
+        <?php
+
+        $flavors = array("grasshopper" => "The Grasshopper",
+                            "maple" => "Whisky Maple Bacon",
+                            "carrot" => "Carrot Walnut",
+                            "caramel" => "Salted Caramel Cupcake",
+                            "velvet" => "Red Velvet",
+                            "lemon" => "Lemon Drop",
+                            "tiramisu" => "Tiramisu");
+
+        foreach($flavors as $key => $value)
+        {
+
+        }
+
+        ?>
+</form>
+
+</body>
+</html>

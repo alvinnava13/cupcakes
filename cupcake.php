00896f321a78bc80fb826e3a3b4be81ea8bb8e98
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-07 00:56:09 -0400

    Created calculate_cost() function to calculate total cost for cupcakes and created an order confirmation message

--- cupcake.php
+++ cupcake.php
@@ -1,6 +1,53 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $errors = [];
 
+    if (empty($_POST['name']))
+    {
+        $errors[] = 'You forgot to enter your name.';
+    }
+
+    if(!isset($_POST['flavor']))
+    {
+        $errors[] = 'You must select at least one flavor.';
+    }
+    if(isset($_POST['flavor']))
+    {
+        $flavor = $_POST['flavor'];
+    }
+
+    if(empty($errors))
+    {
+        echo '<h1>Order Confirmation</h1>';
+        //echo '<p>Thank you, ' . $_POST['name'] . ' ' . 'for your order of ' . implode(", ", $flavor) . ' cupcake</p>';
+        echo '<p>Thank you, ' . $_POST['name'] . ', ' . 'for your order.</p>';
+        echo '<p>Order Summary:</p>';
+
+        echo '<ul>';
+
+        foreach($flavor as $key => $value)
+        {
+            echo "<li>" . $value . "</li>";
+        }
+
+        echo '</ul>';
+        $cost = calculate_cost($flavor);
+        echo '<p>Order Total: $' . $cost . '</p>';
+    }
+
+} // End of IF statement
+
+// Calculate cost of selected cupcakes
+function calculate_cost($flavor)
+{
+    $amount = count($flavor);
+
+    $total = $amount * 3.5;
+
+    return number_format($total, 2);
+}
 
 ?>
 
@@ -19,11 +66,11 @@
 <h1>Cupcake Fundraiser</h1>
 
 <form action="cupcake.php" method="POST" id="form">
-    <label>Your Name: <br><input type="text" name="name" size="50" maxlength="50" value="
+    <label>Your Name: <br><input type="text" name="name" size="30" maxlength="50" value="
     <?php if (isset($_POST['name'])) echo $_POST['name']; ?>"></label><br>
     <label><p>Cupcake Flavors:</p>
         <?php
-
+        // $key => $value
         $flavors = array("grasshopper" => "The Grasshopper",
                             "maple" => "Whisky Maple Bacon",
                             "carrot" => "Carrot Walnut",
